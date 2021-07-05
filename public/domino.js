c8e7f38462b6b681f620c55ce8adc69b4fe40ea1
Glitch (werise-love) <none>
2021-07-05 06:22:36 +0000

    ✋🎟 Checkpoint ./public/domino.js:37582/403

--- public/domino.js
+++ public/domino.js
@@ -4,10 +4,28 @@ class Tile extends Array {
     this[0] = t;
     this[1] = b;
   }
+  
+  get id() {
+    return `${this[0]},${this[1]}`
+  }
 }
 
-const [t, b] = r;
-console.log(t, b);
+class Domino {
+  constructor () {
+    
+  }
+  
+  createTiles () {
+    const tiles = new Map();
+    
+    for (let t = 1; t <= 12; t++) {
+      for (let b = 1; b <= 12; b++) {
+        const tile = new Tile(t, b);
+        tiles.set(tile.id, tile)
+      }
+    }
+  }
+}
 
 if (module) {
   module.exports = {
